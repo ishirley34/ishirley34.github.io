91490a1ccb57bbd9c76f7b96ced64ba40bf59759
ian shirley <ian shirley>
2017-03-30 01:29:43 -0500

    troubleshooting test

--- js/script.js
+++ js/script.js
@@ -41,9 +41,9 @@ jQuery(document).ready(function($){
 	var target = $(this).attr("href");
 			
 
-	// $('html, body').stop().animate({ scrollTop: $(target).offset().top-140 }, 1000, function() {
+	$('html, body').stop().animate({ scrollTop: $(target).offset().top-140 }, 1000, function() {
 
-	// });
+	});
 			
 	return false;
    });
